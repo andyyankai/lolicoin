b2ab2dcce8cc361d1a55b002b92e5a13c3214382
andyyankai <kyan8@uci.edu>
2018-09-09 15:17:19 -0700

    tcp client

--- client.go
+++ client.go
@@ -2,5 +2,19 @@ package main
 
 import (
 	"fmt"
-	"log"
+	"net"
+	"os"
+	"bufio"
 )
+
+func run() {
+	conn, _ := net.Dial("tcp", "localhost:8081")
+	for {
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Print("Text to send: ")
+		text, _ := reader.ReadString('\n')
+		fmt.Fprintf(conn, text + "\n")
+		message, _ := bufio.NewReader(conn).ReadString('\n')
+		fmt.Print("Message from server: " + message)
+	}
+}
