63efef70621f50868a1e26192c3578fa6b473611
andyyankai <kyan8@uci.edu>
2018-09-09 14:45:23 -0700

    server prototype

--- server.go
+++ server.go
@@ -1,3 +1,24 @@
 package main
 
+import (
+	"net"
+	"fmt"
+	"bufio"
+	"strings"
+)
 
+func connect() {
+	fmt.Println("Launching server...")
+	ln, _ := net.Listen("tcp", ":8081")
+
+	conn, _ := ln.Accept()
+
+	for {
+		message, _ := bufio.NewReader(conn).ReadString('\n')
+		fmt.Print("Message Received:", string(message))
+		newmessage := strings.ToUpper(message)
+		conn.Write([]byte(newmessage + "\n"))
+
+	}
+
+}
