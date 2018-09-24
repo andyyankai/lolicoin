aa573f69b68600a41c9d65302f4532401ce56c42
andyyankai <kyan8@uci.edu>
2018-09-24 01:18:01 -0700

    revert to cli

--- main.go
+++ main.go
@@ -1,24 +1,9 @@
 package main
 
-import (
-	"fmt"
-	"strconv"
-)
-
 func main() {
-	bc := NewLolichan()
+	lc := NewLolichan()
+	defer lc.db.Close()
 
-	bc.AddLoli("Send 1 lolicoin to A")
-	bc.AddLoli("Send 2 more lolicoin to A")
-
-	for _, loli := range bc.lolis {
-		fmt.Printf("Prev. hash: %x\n", loli.PrevLoliHash)
-		fmt.Printf("Data: %s\n", loli.Data)
-		fmt.Printf("Hash: %x\n", loli.Hash)
-		fmt.Println()
-		pow := NewProofOfWork(loli)
-		fmt.Printf("PoW: %s\n", strconv.FormatBool(pow.Validate()))
-		fmt.Println()
-	}
+	cli := CLI{lc}
+	cli.Run()
 }
-
